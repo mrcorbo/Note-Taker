a0542b7baa09467af9cbe29eb9e2762b5a6c968b
mrcorbo <109931379+mrcorbo@users.noreply.github.com>
2022-10-16 09:45:43 -0600

    Set up delete function using routes

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,20 +1,35 @@
-const notes = require('express').Router();
+const router = require('express').Router();
 const fs = require('fs');
+const { resourceLimits } = require('worker_threads');
+const { v4:uuidv4 } = require('uuid') 
 
 // GET Route for retrieving all the notes
-notes.get('/', (req, res) => {
+router.get('/', (req, res) => {
     fs.readFile('./db/db.json', "utf-8", (err, data) => {
         res.send(data);
-        
     })
 });
 
 // POST Route for submitting notes
-notes.post('/', (req, res) => {
+router.post('/', (req, res) => {
     fs.readFile('./db/db.json', "utf-8", (err, data) => {
-        res.send(data);
-        
+    const notes = JSON.parse(data);
+    notes.push({...req.body, id:uuidv4()});
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+        res.send(req.body);
+    })    
     })
 });
 
-module.exports = notes;
+// DELETE Route for submitting notes
+router.delete('/:id', (req, res) => {
+    fs.readFile('./db/db.json', "utf-8", (err, data) => {
+    const notes = JSON.parse(data);
+    const filteredNotes = notes.filter (note => note.id !== req.params.id);
+    fs.writeFile('./db/db.json', JSON.stringify(filteredNotes), (err) => {
+        res.send(200);
+    })    
+    })
+});
+
+module.exports = router;
